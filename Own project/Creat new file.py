130ed801642e2c8bd93b79cfd416d0b9f3b58a49
AnonymousTYMON <tymoncar@gmail.com>
2022-03-19 17:51:56 +1100

    False

--- Own project/Creat new file.py
+++ Own project/Creat new file.py
@@ -1,13 +1,13 @@
 import os
-'''
-for n in range(n1, n2):
-    par_dir = "(Location)"
+
+for n in range(0, 40):
+    par_dir = "C:/Users\Tymon\Pictures\給100分\畫家\荻pote (ogipote)"
     dir = str(n)
     path = os.path.join(par_dir, dir)
     mode = 0o666
     os.mkdir(path, mode)
-'''
 
+'''
 par_dir = "(Location)"
 dir = "(New folder name)"
 path = os.path.join(par_dir, dir)
@@ -15,3 +15,4 @@ mode = 0o666
 os.mkdir(path, mode)
 
 #C:/Users\Tymon\Pictures\給100分
+'''
