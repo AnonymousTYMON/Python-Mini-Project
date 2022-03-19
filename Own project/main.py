db9a13096f1d6ff2a2d80aeb59dcc3a277f9d053
AnonymousTYMON <95165584+AnonymousTYMON@users.noreply.github.com>
2022-03-19 18:02:49 +1100

    Simpl3

---
+++ Own project/main.py
@@ -0,0 +1,10 @@
+#Create a Python project to perform some simple statistics on a list of values.
+#Click me to see the sample solution
+import stats
+lst     = [6, 6, 10, 15 , 9 , 8, 17, 5]
+Stats   = stats.Stats
+#Need data: Min, max, average, range, mean, mode, median, 
+#Inter-quartile range, standard deviation, standard score
+def min(lst):
+    return min
+    
